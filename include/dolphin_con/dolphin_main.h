cc2bf2d8373eda6ce67802b2477ad03d9f3d1255
shines77 <gz_shines@msn.com>
2013-12-03 08:08:49 +0800

    Update the projects for vc2010, and add a few vc2010 ignore file extensions to .gitgnore. (更新了vc2010的项目文件, .gitgnore添加了几个vc2010的忽略文件扩展名)
    Signed-off-by: shines77 <gz_shines@msn.com>

---
+++ include/dolphin_con/dolphin_main.h
@@ -0,0 +1,25 @@
+
+#ifndef _DOLPHIN_MAIN_H_
+#define _DOLPHIN_MAIN_H_
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// some declear
+
+#ifdef __cplusplus
+}
+#endif
+
+namespace dolphin_console {
+
+// some functions
+
+}  // namespace dolphin_console
+
+#endif // _DOLPHIN_MAIN_H_
